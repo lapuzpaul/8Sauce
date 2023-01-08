22c8567c143531c3d43fbc4db32af220e4909c06
Javes1441 <jgblb14@gmail.com>
2023-01-08 16:54:01 +0800

    product page done

---
+++ js/product_JS/product.js
@@ -0,0 +1,7 @@
+function product_click() {
+    var image = document.getElementsByClassName("product-img-1");
+
+    image.src = "../../img/product-img/Bird Booster Chicken Injection Chipotle/Bird Booster Chipotle-1.png";
+
+    document.write("hello")
+}
