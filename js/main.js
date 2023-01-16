fcb137c9d02be17379b5ba530a1cb38b1f9e7e75
emaniago <edmonmaniago2@gmail.com>
2023-01-17 05:52:04 +0800

    added images on shop page

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ items.forEach((el) => {
     let next = el.nextElementSibling
     for (var i=1; i<minPerSlide; i++) {
         if (!next) {
-            // wrap carousel by using first child
         	next = items[0]
       	}
         let cloneChild = next.cloneNode(true)
